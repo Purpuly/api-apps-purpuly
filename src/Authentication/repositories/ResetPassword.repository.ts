ef2bf2c79519b61c75737fa372bcf746277a2efe
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-06-16 01:32:07 +0200

    chore: Update ResetPasswordRepository with new methods

--- src/Authentication/repositories/ResetPassword.repository.ts
+++ src/Authentication/repositories/ResetPassword.repository.ts
@@ -1,5 +1,6 @@
 export default abstract class ResetPasswordRepository {
-    abstract resetPasswordTokenIsValid(token: string): Promise<boolean>;
+    abstract requestPasswordReset(email: string): Promise<boolean>;
+    abstract checkIfResetPasswordTokenIsValid(token: string): Promise<boolean>;
     abstract invalidateResetPasswordToken(token: string): Promise<{ success: boolean, invalidated_token: string }>;
     abstract generatePasswordResetToken(email: string): Promise<string>;
 }
