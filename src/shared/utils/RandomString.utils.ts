191ed7c2a541d5f4ce1edcd4c027d354aab96829
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-06-16 03:30:56 +0200

    feat: Add RandomString utility class for generating random strings

---
+++ src/shared/utils/RandomString.utils.ts
@@ -0,0 +1,11 @@
+import { randomBytes } from 'node:crypto';
+
+const DEFAULT_STRING_LENGTH: number = 64;
+
+export default class RandomString {
+    public static generate(
+        string_length: number = DEFAULT_STRING_LENGTH
+    ): string {
+        return randomBytes(string_length).toString('hex');
+    }
+}
