7c0ebbfa80c0919af22a1952460c907caf481d87
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-06-17 09:02:21 +0200

    refactor: Update ResetPasswordRepository method name for checking reset password token validity and expiration

--- src/core/repositories/Authentication/Password.repository.ts
+++ src/core/repositories/Authentication/Password.repository.ts
@@ -1,4 +1,4 @@
 export default abstract class PasswordRepository {
-    abstract checkIfPasswordPassesPasswordPolicies(password: string): Promise<boolean>;
-    abstract validateAndUpdatePassword(user_id: string, new_password: string): Promise<void>;
+    abstract checkIfPasswordPassesPasswordPoliciesForApp(app_id: string, password: string): Promise<boolean>;
+    abstract validateAndUpdatePasswordForApp(app_id: string, user_id: string, new_password: string): Promise<void>;
 }
--- src/core/repositories/Authentication/ResetPassword.repository.ts
+++ src/core/repositories/Authentication/ResetPassword.repository.ts
@@ -1,5 +1,5 @@
 export default abstract class ResetPasswordRepository {
-    abstract checkIfResetPasswordTokenIsValid(app_id: string, user_id: string, token: string): Promise<boolean>;
-    abstract invalidateResetPasswordTokenForApp(app_id: string, user_id: string): Promise<{ success: boolean, invalidated_token: string }>;
+    abstract checkIfResetPasswordTokenIsValidAndNotExpired(app_id: string, request_id: string, token: string): Promise<boolean>;
+    abstract invalidateResetPasswordTokenForApp(app_id: string, request_id: string): Promise<{ success: boolean, invalidated_token: string }>;
     abstract saveResetPasswordTokenForApp(app_id: string, user_id: string, token: string): Promise<boolean>;
 }
