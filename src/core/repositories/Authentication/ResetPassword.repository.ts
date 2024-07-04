dcad2d439dddfd8dc352cdc0ff5f066a59a29eca
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-07-04 18:07:44 +0200

    refactor: Update ResetPasswordRepository to use ResetPasswordRecord for token validation and invalidation

--- src/core/repositories/Authentication/ResetPassword.repository.ts
+++ src/core/repositories/Authentication/ResetPassword.repository.ts
@@ -1,6 +1,8 @@
+import type ResetPasswordRecord from "@shared/interfaces/reset-password-record.type";
+
 export default abstract class ResetPasswordRepository {
-    abstract checkIfResetPasswordTokenIsValidAndNotExpired(app_id: string, request_id: string, token: string): Promise<boolean>;
-    abstract getApplicationUserIdFromRequestIdAndResetPasswordToken(app_id: string, request_id: string, token: string): Promise<string>;
-    abstract invalidateResetPasswordTokenForApp(app_id: string, request_id: string): Promise<{ success: boolean, invalidated_token: string }>;
+    abstract getResetPasswordRecordFromRecordId(record_id: string): Promise<ResetPasswordRecord | null>;
+    abstract checkIfResetPasswordTokenIsValidAndNotExpired(record: ResetPasswordRecord): Promise<boolean>;
+    abstract invalidateResetPasswordToken(record: ResetPasswordRecord): Promise<{ success: boolean, invalidated_token: string }>;
     abstract saveResetPasswordTokenForApp(app_id: string, user_id: string, token: string): Promise<boolean>;
 }
