becd94fb9116ec51719b3a452a9ec61f0d850edf
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-07-04 18:08:27 +0200

    refactor: Add ResetPasswordAdapters class for converting between ResetPassword and ResetPasswordRecord

---
+++ src/shared/adapters/ResetPassword.adapters.ts
@@ -0,0 +1,29 @@
+import type { ResetPassword } from "@prisma/client";
+import type ResetPasswordRecord from "@shared/interfaces/reset-password-record.type";
+
+const DEFAULT_RESET_PASSWORD_EXPIRATION_TIME_IN_MINUTES: number = 15;
+
+export default class ResetPasswordAdapters {
+    public static fromResetPasswordToResetPasswordRecord(
+        reset_password: ResetPassword
+    ): ResetPasswordRecord {
+        return {
+            recordId: reset_password.id,
+            userId: reset_password.user_id,
+            appId: reset_password.app_id,
+            resetPasswordToken: reset_password.token,
+            isExpired: ResetPasswordAdapters.checkIfIsExpired(reset_password.iat),
+        };
+    }
+
+    private static checkIfIsExpired(
+        timestamp: number,
+        expirationTimeInMinutes: number = DEFAULT_RESET_PASSWORD_EXPIRATION_TIME_IN_MINUTES
+    ): boolean {
+        const now = Date.now();
+
+        const expirationTimeInMilliseconds = expirationTimeInMinutes * 60 * 1000;
+
+        return Number(now) >= Number(timestamp + expirationTimeInMilliseconds);
+    }
+}
