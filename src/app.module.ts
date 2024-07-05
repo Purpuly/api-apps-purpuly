ec75f3cb414fb1311c576b38ba19df6921341b97
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-07-05 17:48:28 +0200

    feat: Add global configuration module for NestJS application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import routes from './app.routes';
 import ApiModule from './api.module';
 import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     RouterModule.register(routes),
     ApiModule,
   ],
