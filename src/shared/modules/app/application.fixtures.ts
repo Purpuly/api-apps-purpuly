841f51f7672b1bd0531ccaf3222e8784dd7a06cb
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-07-05 02:20:57 +0200

    refactor: Update application adapter to use camel case for isActive property

--- src/shared/modules/app/application.fixtures.ts
+++ src/shared/modules/app/application.fixtures.ts
@@ -3,10 +3,10 @@ import { Application } from "@prisma/client";
 const applications: Application[] = [
     {
         id: "test_id",
-        name: "Test Application",
-        description: "This is a test application",
-        image_url: "https://example.com/logo.png",
-        url: "https://example.com",
+        name: "Admin Magic Hands",
+        description: "Plateforme de gestion des rendez-vous de Magic Hands",
+        image_url: "https://magichands-massage.fr/apple-icon.png",
+        url: "https://admin.magichands-massage.fr",
         created_at: new Date(),
         is_active: true,
         status: "approved",
