077185af9590ead58c28dec9c341fdda9b255a66
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-07-09 01:20:13 +0200

    feat: Add successCodes module for handling success response codes

---
+++ src/shared/success/success-codes.ts
@@ -0,0 +1,8 @@
+const successCodes = {
+    resetPassword: {
+        mutation: 'success/reset-password/mutation',
+        request: 'success/reset-password/request',
+    },
+};
+
+export default successCodes;
