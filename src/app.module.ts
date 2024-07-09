e010014e3de6154f7ff06ad4335e596e76688265
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-07-09 09:48:26 +0200

    feat: Add ThrottlerModule for API rate limiting functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import SharedModule from '@shared/shared.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 10,
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
