dcb6c297bafc8a8e6c912b8d8efb451123013525
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-08-17 23:54:32 +0200

    feat: Add WebhookEgressUtils class for triggering webhooks

---
+++ src/shared/utils/WebhookEgress.utils.ts
@@ -0,0 +1,38 @@
+import { createHmac } from "node:crypto";
+import axios, { AxiosInstance } from "axios";
+
+const HMAC_SIGNATURE_ALGORITHM: string = "sha256";
+const HMAC_SIGNATURE_HEADER: string = "X-Signature";
+const WEBHOOK_EGRESS_UA: string = "purpuly-webhook-egress/1.0";
+
+const baseWebhookEgressInstance: AxiosInstance = axios.create({
+    headers: {
+        "Content-Type": "application/json",
+        "X-Powered-By": "Purpuly Apps",
+        "User-Agent": WEBHOOK_EGRESS_UA,
+    },
+});
+
+export default class WebhookEgressUtils {
+    public static async triggerWebhook(
+        url: string,
+        secret: string,
+        data: any
+    ): Promise<void> {
+        const signature: string =
+            WebhookEgressUtils.createHMACSignature(secret, JSON.stringify(data));
+
+        await baseWebhookEgressInstance.post(url, data, {
+            headers: {
+                [HMAC_SIGNATURE_HEADER]: signature,
+            },
+        });
+    }
+
+    private static createHMACSignature(
+        secret: string,
+        data: string
+    ): string {
+        return createHmac(HMAC_SIGNATURE_ALGORITHM, secret).update(data).digest("hex");
+    }
+}
