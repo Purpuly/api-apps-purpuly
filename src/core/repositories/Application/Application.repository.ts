0abad58366fb91b9dcaa8fdede81f8cf54aa08dc
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-08-17 23:22:07 +0200

    refactor: Remove unused Security module files

--- src/core/repositories/Application/Application.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from "@prisma/client";
-import PublicApplication from "@shared/interfaces/public-application.type";
-
-export default abstract class ApplicationRepository {
-    abstract getApplicationFromAppId(app_id: string): Promise<Application>;
-    abstract extractPublicApplicationInformationsFromRequest(): Promise<PublicApplication>;
-}
--- src/modules/Security/security.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import SecurityController from "./security.controller";
-import PublicApplicationSecurityService from "./services/public-application-security.service";
-
-@Module({
-    providers: [PublicApplicationSecurityService],
-    controllers: [SecurityController],
-})
-export default class SecurityModule { }
