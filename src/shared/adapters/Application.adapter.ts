4041b440c44d2c0d843d77333cf74ae294e5f5ae
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-08-18 20:55:35 +0200

    feat: Add method to convert Application to ApplicationWebhookPrivate

--- src/shared/adapters/Application.adapter.ts
+++ src/shared/adapters/Application.adapter.ts
@@ -1,4 +1,5 @@
 import type { Application } from "@prisma/client";
+import type ApplicationWebhookPrivate from "@shared/interfaces/application-webhook-private.type";
 import type PublicApplication from "@shared/interfaces/public-application.type";
 
 export default class ApplicationAdapter {
@@ -13,6 +14,16 @@ export default class ApplicationAdapter {
             logoUrl: application.image_url,
             isActive: application.is_active,
             webhookFeatureIsEnabled: application.security_webhook_is_enabled,
+        };
+    }
+
+    public static fromApplicationToApplicationWebhookPrivate(
+        application: Application
+    ): ApplicationWebhookPrivate {
+        return {
+            security_webhook_is_enabled: application.security_webhook_is_enabled,
+            security_webhook_secret: application.security_webhook_secret,
+            security_webhook_url: application.security_webhook_url,
         };
     }
 };
