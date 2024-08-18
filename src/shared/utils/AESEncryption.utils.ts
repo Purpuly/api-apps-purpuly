4c74de17249146c32075541326d7bcc3eb29a1ff
Ousmane Mbaye <mbayeousmane900@gmail.com>
2024-08-18 20:55:12 +0200

    fix: Fix AES encryption utility class to handle exceptions and log errors

--- src/shared/utils/AESEncryption.utils.ts
+++ src/shared/utils/AESEncryption.utils.ts
@@ -1,26 +1,55 @@
-import { createCipheriv, createDecipheriv, randomBytes } from "crypto";
+import { HttpException, Logger } from "@nestjs/common";
+import { createCipheriv, createDecipheriv, randomBytes, createHash } from "crypto";
 
-const IV_LENGTH: number = 16;
+const DEFAULT_INITIAL_VECTOR_LENGTH: number = 16;
+const IV_SEPARATOR: string = ":";
 const AES_ALGORITHM: string = "aes-256-cbc";
 
 export default class AESEncryptionUtils {
     public static encrypt(data: string, secret: string): string {
-        const iv: Buffer = randomBytes(IV_LENGTH);
-        const cipher = createCipheriv(AES_ALGORITHM, secret, iv);
+        try {
+            const iv = randomBytes(DEFAULT_INITIAL_VECTOR_LENGTH);
+            const cipher = createCipheriv(AES_ALGORITHM, secret, iv);
 
-        let encrypted = cipher.update(data, "utf-8", "hex");
-        encrypted += cipher.final("hex");
+            let encrypted = cipher.update(data, 'utf8', 'hex');
+            encrypted += cipher.final('hex');
 
-        return `${iv.toString("hex")}:${encrypted}`;
+            const stringifiedIv = iv.toString('base64');
+
+            return `${stringifiedIv}${IV_SEPARATOR}${encrypted}`;
+        } catch (error) {
+            Logger.error("AES encryption failed", error);
+            throw new HttpException(
+                "Encryption failed, aborting operation, we just prevented data corruption and send a notification to the development team to investigate the issue.",
+                500
+            );
+        }
     }
 
     public static decrypt(data: string, secret: string): string {
-        const [iv, encryptedData] = data.split(":");
-        const decipher = createDecipheriv(AES_ALGORITHM, secret, Buffer.from(iv, "hex"));
+        try {
+            const [stringifiedIv, encrypted] = data.split(IV_SEPARATOR);
 
-        let decrypted = decipher.update(encryptedData, "hex", "utf-8");
-        decrypted += decipher.final("utf-8");
+            const iv = Buffer.from(stringifiedIv, 'base64');
+            const decipher = createDecipheriv(AES_ALGORITHM, secret, iv);
 
-        return decrypted;
+            let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+            decrypted += decipher.final('utf8');
+
+            return decrypted;
+        } catch (error) {
+            Logger.error("AES decryption failed", error);
+            throw new HttpException(
+                "Decryption failed, aborting operation, we just prevented data corruption and send a notification to the development team to investigate the issue.",
+                500
+            );
+        }
+    }
+
+    public static generate32BytesSha256KeyFromString(value: string): string {
+        return createHash('sha256')
+            .update(String(value))
+            .digest('base64')
+            .substring(0, 32);
     }
 }
